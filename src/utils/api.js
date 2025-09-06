bdfb51df68b2238aac94da89ad18fe9cd546863f
priya-saran12345 <ps2297404@gmail.com>
2025-09-06 14:24:48 +0530

    jhjjjj

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://loan-management-backend.vercel.app/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api/',
   withCredentials: true, // send/receive cookies
 });
 
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// // https://vite.dev/config/
+// // export default defineConfig({
+// //   plugins: [react(), tailwindcss(),],
+// // })
+// export default defineConfig({
+//   plugins: [react(), tailwindcss(),],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:4000',
+//         changeOrigin: true,
+//       }
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss(),],
-// })
-
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-      }
-    }
-  }
+  plugins: [react(), tailwindcss()]
 })
-
